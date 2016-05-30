0ee450dd4c247a87eccb0cb7e6a37937864cf956
Daniel Chao <daniel@danielchao.me>
2016-05-29 21:38:17 -0600

    Un-bootstrap-ify navigation bar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,26 +1,24 @@
-import React, {Component} from 'react'
-import {Navbar, Nav, NavItem} from 'react-bootstrap'
-import {Link} from 'react-router'
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+import '../styles/nav';
 
-export default class NavigationBar extends Component {
-    constructor(props){
-        super(props);
-    }
-    
-    render(){
-        return(
-            <Navbar>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <Link to="/">daniel chao</Link>
-                    </Navbar.Brand>
-                </Navbar.Header>
-                <Nav pullRight>
-                    <NavItem eventKey={1} href="/">home</NavItem>
-                    <NavItem eventKey={2} href="/about">about me</NavItem>
-                    <NavItem eventKey={3} href="/work">my work</NavItem>
-                </Nav>
-            </Navbar>            
-        )
+class NavigationBar extends Component {
+    render() {
+        return (
+            <nav className="navigation">
+                <Link className="nameHeader" to="/">daniel chao</Link>
+                <div className="headerLinks">
+                    <ul className="headerList">
+                        <li className="navListItem"><Link to="/" className="navItem">home</Link></li>
+                        <li className="navListItem"><Link to="/about" className="navItem">about me</Link></li>
+                        <li className="navListItem"><Link to="/projects" className="navItem">projects</Link></li>
+                        <li className="navListItem"><Link to="https://github.com/dchao19/" className="navItem">github</Link></li>
+                        <li className="navListItem"><Link to="/connect" className="navItem">let's talk</Link></li>
+                    </ul>
+                </div>
+            </nav>
+        );
     }
 }
+
+export default NavigationBar;
